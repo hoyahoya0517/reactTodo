b557ecb255f3964d237b3067f177b9e3e76526ae
이건호 <hoyahoya0517@naver.com>
2022-12-30 03:12:47 +0900

    mob

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
 import Todo from "./Todo";
 
 function App() {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`); //"--vh"라는 속성으로 정의해준다.
+  }
+
+  window.addEventListener("resize", () => setScreenSize());
+  useEffect(() => {
+    setScreenSize();
+  }); //처음 마운트될때 값을 계산하도록 함수를 호출한다
   return (
     <>
       <Todo />
